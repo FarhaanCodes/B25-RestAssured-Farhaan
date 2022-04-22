fbd0a6c77338e11e261f33d9272de79bf34a0c61
FarhaanCodes <farhaan360@gmail.com>
2022-04-22 05:19:00 -0500

    list and show how to show values in path

--- src/test/java/com/cydeo/day3/SpartanTestsWithPath.java
+++ src/test/java/com/cydeo/day3/SpartanTestsWithPath.java
@@ -1,4 +1,94 @@
 package com.cydeo.day3;
 
-public class SpartanTestsWithPath {
+import com.cydeo.utillities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpartanTestsWithPath extends SpartanTestBase {
+
+         /*
+     Given accept type is json
+     And path param id is 10
+     When user sends a get request to "api/spartans/{id}"
+     Then status code is 200
+     And content-type is "application/json"
+     And response payload values match the following:
+          id is 10,
+          name is "Lorenza",
+          gender is "Female",
+          phone is 3312820936
+   */
+
+    @DisplayName("GET one spartan with Path")
+    @Test
+    public void test1() {
+
+
+        Response response = given().log().all()
+                .accept(ContentType.JSON)
+                .and().pathParam("id", 10)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.contentType());
+
+        //verify each json key has specific values
+        System.out.println(response.path("id").toString());
+        System.out.println(response.path("name").toString());
+        System.out.println(response.path("gender").toString());
+        System.out.println(response.path("phone").toString());
+
+
+        //save values into variables
+        int id = response.path("id");
+        String name = response.path("name");
+        String gender = response.path("gender");
+        long phone = response.path("phone");
+
+
+        //verify each
+        assertEquals(10, id);
+        assertEquals("Lorenza", name);
+        assertEquals("Female", gender);
+        assertEquals(3312820936l, phone);
+    }
+
+    @DisplayName("GET all spartan and navigate with Path()")
+    @Test
+    public void test2() {
+
+        Response response = given().
+                            accept(ContentType.JSON).
+                            when().get("/api/spartans");
+
+
+        int firstID = response.path("id[0]");
+        System.out.println("firstID = " + firstID);
+
+        String name = response.path("name[0]");
+        System.out.println("name = " + name);
+
+        String lastFirstName = response.path("name[-1]");
+        System.out.println("lastFirstName = " + lastFirstName);
+
+        //how to get all names and store inside the list?
+
+        List<String> names = response.path("name");
+
+        System.out.println(names);
+
+        //print names one by one?
+
+        for (String s : names){
+            System.out.println(s);
+        }
+    }
 }
