f040c9489f244b836cdee37eddc512eb1421e16c
FarhaanCodes <farhaan360@gmail.com>
2022-04-27 08:23:13 -0500

    day 5

---
+++ src/test/java/com/cydeo/pojo/Employee.java
@@ -0,0 +1,26 @@
+package com.cydeo.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data // from lombok, getter,setter,tostring together
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Employee {
+
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    @JsonProperty("job_id")
+    private String jobId;
+    private int salary;
+
+
+
+
+
+
+}
---
+++ src/test/java/com/cydeo/pojo/Link.java
@@ -0,0 +1,12 @@
+package com.cydeo.pojo;
+
+
+import lombok.*;
+
+@Data
+public class Link {
+
+    private String rel;
+    private String href;
+}
+
