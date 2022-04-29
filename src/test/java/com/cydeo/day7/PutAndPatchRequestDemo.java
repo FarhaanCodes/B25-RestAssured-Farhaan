7025bd6e79683e14fb8b6534dc99e765a7d7ec2d
FarhaanCodes <farhaan360@gmail.com>
2022-04-29 18:59:55 -0500

    day 6

---
+++ src/test/java/com/cydeo/day7/PutAndPatchRequestDemo.java
@@ -0,0 +1,22 @@
+package com.cydeo.day7;
+
+import com.cydeo.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class PutAndPatchRequestDemo extends SpartanTestBase {
+
+
+
+    @DisplayName("PUT request to one spartan for update with Map")
+    @Test
+    public void test1(){
+
+
+    }
+}
+
